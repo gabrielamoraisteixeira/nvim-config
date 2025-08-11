0ae09a5902837ad91a8b280402ab3a84b18cbabe
gabrielamoraisteixeira <gabrielamoteixeira@gmail.com>
2025-08-11 12:07:00 +0100

    feat: toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    open_mapping = [[<leader>tt]],
+    direction = "float",
+    auto_scroll = true,
+    float_opts = {
+      border = "curved",
+    },
+  },
+  config = true,
+}
